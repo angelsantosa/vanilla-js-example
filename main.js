7590107bd37b7830df33fd8e323d49f54f5cb4e8
GangrelWolf <xynavarrete@gmail.com>
2018-03-16 22:42:56 -0600

    Images
    Stuff

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ var welcome_msg = document.getElementById('welcome_msg');
 
 var msg_blue = "blue-header";
 
+var dog_pictures = ["imagen/beagle.jpeg", "imagen/german.jpeg", "imagen/husky.jpeg", "imagen/collie.jpeg"]
+
+$("#dog_click").click(function() {
+  var dog = $(this).attr('dog');
+  console.log(dog);
+});
+
 boton.onclick = function() {
   welcome_msg.classList.add(msg_blue);
+
+
 };
