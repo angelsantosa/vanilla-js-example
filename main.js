3f67347d7f18b00234a999f158185ead041e8c48
GangrelWolf <xynavarrete@gmail.com>
2018-03-16 23:10:53 -0600

    Works
    How it works, I have a somewhat faint idea, but an idea, and IT WORKS.

--- main.js
+++ main.js
@@ -5,13 +5,27 @@ var welcome_msg = document.getElementById('welcome_msg');
 
 var msg_blue = "blue-header";
 
-var dog_pictures = ["imagen/beagle.jpeg", "imagen/german.jpeg", "imagen/husky.jpeg", "imagen/collie.jpeg"]
+var dog_pictures = ["imagen/beagle.jpg", "imagen/german.jpg", "imagen/husky.jpg", "imagen/collie.jpg"]
 
-$("#dog_click").click(function() {
+$("a#dog_click").click(function() {
   var dog = $(this).attr('dog');
-  console.log(dog);
-});
+  if(dog == "1"){
+    $("#lol").attr('src',dog_pictures[0]);
+  }
 
+  if(dog == "2"){
+    $("#lol").attr('src',dog_pictures[2]);
+  }
+
+  if(dog == "3"){
+    $("#lol").attr('src',dog_pictures[3]);
+  }
+
+  if(dog == "4"){
+
+    $("#lol").attr('src',dog_pictures[1]);
+  }
+});
 
 boton.onclick = function() {
   welcome_msg.classList.add(msg_blue);
